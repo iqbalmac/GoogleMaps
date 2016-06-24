4b6196c7e5af7f3f4edf36edb5eee22262615503
Sethu <Sethu@sethus-imac.local>
2016-06-24 12:16:06 +0530

    Initial Commit

---
+++ ExampleOfgoogleMaps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExampleOfgoogleMaps
+//
+//  Created by Sethu on 24/06/16.
+//  Copyright © 2016 kaizen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExampleOfgoogleMaps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExampleOfgoogleMaps
+//
+//  Created by Sethu on 24/06/16.
+//  Copyright © 2016 kaizen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
